14fb2c32868cacf769f3f3e84c64729cd0252e27
bernardo.nascimento <bernardonasciimento@gmail.com>
2021-04-16 13:17:27 -0300

    Web Scraping

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const puppeteer = require('puppeteer')
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+  const date = new Date()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.reclameaqui.com.br/empresa/lottocap/')
+
+  const avg = await page.evaluate(() => {
+    const classScore = document.getElementsByClassName('score')
+    return classScore[0].firstChild.innerHTML
+  })
+
+  await browser.close()
+
+  res.setHeader('Cache-control', 's-max-age=60, stable-while-revalidate')
+  res.status(200).json({ 
+    date: date.toUTCString(),
+    avg
+  })
 }
